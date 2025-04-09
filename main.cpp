572602cd544cfb7bc2df51cccc186a59fde99b6c
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-04-09 22:20:46 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,102 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    #include <iostream>
+    #include <string>
+    #include <ctime>
+    
+    using namespace std;
+    
+   
+    string toString (double);
+    int toInt (string);
+    double toDouble (string);
+    
+    int main() {
+        srand(time(0));   
+        
+        bool scambio;
+        int n, i, f, g, j, t, s;
+    
+        cout << "quanti numeri generare?" << endl;
+        cin >> n;
+        int p[n], d[n], v[n];
+    
+        i = 0;
+        while (i < n) {
+            v[i] = rand() % n;
+            i = i + 1;
+        }
+        i = 0;
+        f = 0;
+        g = 0;
+        while (i < n) {
+            if (v[i] % 2 == 0) {
+                p[f] = v[i];
+                f = f + 1;
+            } else {
+                d[g] = v[i];
+                g = g + 1;
+            }
+            i = i + 1;
+        }
+        scambio = true;
+        i = 0;
+        while (i <= g - 1 && scambio) {
+            scambio = false;
+            j = 0;
+            while (j <= g - 2 - i) {
+                if (d[j] > d[j + 1]) {
+                    t = d[j];
+                    d[j] = d[j + 1];
+                    d[j + 1] = t;
+                    scambio = true;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+        scambio = true;
+        i = 0;
+        while (i <= f - 1 && scambio) {
+            scambio = false;
+            j = 0;
+            while (j <= f - 2 - i) {
+                if (p[j] > p[j + 1]) {
+                    t = p[j];
+                    p[j] = p[j + 1];
+                    p[j + 1] = t;
+                    scambio = true;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
+        i = 0;
+        f = 0;
+        g = 0;
+        while (i < f) {
+            cout << p[i] << endl;
+            i = i + 1;
+        }
+        i = 0;
+        while (i < g) {
+            cout << d[i] << endl;
+            i = i + 1;
+        }
+        i = g;
+        while (i > 0) {
+            cout << d[i] << endl;
+            i = i - 1;
+        }
+        return 0;
+    }
+    
+   
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
